9f7d13a662eb7623cc1423ae7f11b001e6e9729f
Jay Kola <jay@noesisinformatica.com>
2017-03-12 21:09:37 +0000

    feat: #2: Updated operino detail component to include logic for toggling on and off using ui-switch

--- src/main/webapp/app/entities/operino/operino-detail.component.ts
+++ src/main/webapp/app/entities/operino/operino-detail.component.ts
@@ -1,8 +1,11 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { JhiLanguageService } from 'ng-jhipster';
+import { Response } from '@angular/http';
+
+import { EventManager, AlertService, JhiLanguageService } from 'ng-jhipster';
 import { Operino } from './operino.model';
 import { OperinoService } from './operino.service';
+import { User, UserService } from '../../shared';
 
 @Component({
     selector: 'jhi-operino-detail',
@@ -12,11 +15,14 @@ export class OperinoDetailComponent implements OnInit, OnDestroy {
 
     operino: Operino;
     private subscription: any;
+    authorities: any[];
+    isSaving: boolean;
 
     constructor(
         private jhiLanguageService: JhiLanguageService,
         private operinoService: OperinoService,
-        private route: ActivatedRoute
+        private route: ActivatedRoute,
+        private alertService: AlertService
     ) {
         this.jhiLanguageService.setLocations(['operino']);
     }
@@ -32,6 +38,37 @@ export class OperinoDetailComponent implements OnInit, OnDestroy {
             this.operino = operino;
         });
     }
+
+    updateStatus() {
+        this.operino.active = ! this.operino.active;
+        this.save();
+    }
+
+    save () {
+        this.isSaving = true;
+        if (this.operino.id !== undefined) {
+            this.operinoService.update(this.operino)
+                .subscribe((res: Operino) => this.onSaveSuccess(res), (res: Response) => this.onSaveError(res.json()));
+        } else {
+            this.operinoService.create(this.operino)
+                .subscribe((res: Operino) => this.onSaveSuccess(res), (res: Response) => this.onSaveError(res.json()));
+        }
+    }
+
+    private onSaveSuccess (result: Operino) {
+        //this.eventManager.broadcast({ name: 'operinoListModification', content: 'OK'});
+        this.isSaving = false;
+    }
+
+    private onSaveError (error) {
+        this.isSaving = false;
+        this.onError(error);
+    }
+
+    private onError (error) {
+        this.alertService.error(error.message, null, null);
+    }
+
     previousState() {
         window.history.back();
     }
