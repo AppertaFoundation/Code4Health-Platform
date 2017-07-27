827d634fde12a1011a1ce7a6165d593fb16121ec
Jay Kola <jay@noesisinformatica.com>
2017-07-27 20:10:26 +0100

    bug:  FIXED: Creating EHR now split into POST and PUT as work around for ThinkEhr issue

--- src/main/java/cloud/operon/platform/service/util/ThinkEhrRestClient.java
+++ src/main/java/cloud/operon/platform/service/util/ThinkEhrRestClient.java
@@ -71,24 +71,8 @@ public class ThinkEhrRestClient {
                 .queryParam("subjectId", subjectId)
                 .queryParam("commiterName", commiterName);
 
-        // read body from existing json template and fill in values
-        InputStream inputStream = ThinkEhrRestClient.class.getClassLoader().getResourceAsStream("sample_requests/ehrStatusBody.json");
-        BufferedReader bufReader = new BufferedReader(new InputStreamReader(inputStream, StandardCharsets.UTF_8));
-        StringBuilder sb = new StringBuilder();
-        String line = bufReader.readLine();
-        while(line != null){
-            sb.append(line).append("\n");
-            line = bufReader.readLine();
-        }
-
-        // now build string
-        String bodyString = sb.toString();
-        // now update data with values from patient
-        bodyString = bodyString.replaceAll("<subjectId>", subjectId);
-        bodyString = bodyString.replaceAll("<subjectNamespace>", subjectNamespace);
-        bodyString = bodyString.replaceAll("<gender>", patient.getGender());
-        bodyString = bodyString.replaceAll("<birth_year>", String.valueOf(patient.getDateOfBirth().getYear()));
-        HttpEntity<Object> request = new HttpEntity<>(bodyString, httpHeaders);
+        // create patient and then update it - doing a single post to create wont set all additional details
+        HttpEntity<Object> request = new HttpEntity<>(httpHeaders);
         log.debug("request = " + request);
 
         ResponseEntity<Map> responseEntity = restTemplate.exchange(
@@ -100,7 +84,47 @@ public class ThinkEhrRestClient {
         log.debug("responseEntity.getBody() = {}", responseEntity.getBody());
         Map response = responseEntity.getBody();
         if(responseEntity.getStatusCode() == HttpStatus.CREATED) {
-            return response.get("ehrId").toString();
+            String ehrId = response.get("ehrId").toString();
+            log.debug("ehrId = {}", ehrId);
+
+            // read body from existing json template and fill in values
+            InputStream inputStream = ThinkEhrRestClient.class.getClassLoader().getResourceAsStream("sample_requests/ehrStatusBody.json");
+            BufferedReader bufReader = new BufferedReader(new InputStreamReader(inputStream, StandardCharsets.UTF_8));
+            StringBuilder sb = new StringBuilder();
+            String line = bufReader.readLine();
+            while(line != null){
+                sb.append(line).append("\n");
+                line = bufReader.readLine();
+            }
+
+            // now build string for put request
+            String bodyString = sb.toString();
+            // now update data with values from patient
+            bodyString = bodyString.replaceAll("<subjectId>", subjectId);
+            bodyString = bodyString.replaceAll("<subjectNamespace>", subjectNamespace);
+            bodyString = bodyString.replaceAll("<gender>", patient.getGender());
+            bodyString = bodyString.replaceAll("<birth_year>", String.valueOf(patient.getDateOfBirth().getYear()));
+            request = new HttpEntity<>(bodyString, httpHeaders);
+            log.debug("Ehr PUT request = " + request);
+
+            // PUT call to ehr/status/
+            builder = UriComponentsBuilder.fromHttpUrl(baseUrl + "ehr/status/" + ehrId);
+
+            responseEntity = restTemplate.exchange(
+                    builder.build().encode().toUri(),
+                    HttpMethod.PUT,
+                    request,
+                    Map.class);
+            log.debug("EHR PUT responseEntity = {}", responseEntity);
+            log.debug("EHR PUT responseEntity.getBody() = {}", responseEntity.getBody());
+            response = responseEntity.getBody();
+            if(responseEntity.getStatusCode() == HttpStatus.OK) {
+                Map<String, String> meta = (Map<String, String>) response.get("meta");
+                String href = meta.get("href");
+                return href.substring(href.lastIndexOf('/') + 1);
+            } else {
+                throw new RuntimeException("Unable to create ehr");
+            }
         } else {
             throw new RuntimeException("Unable to create ehr");
         }
